c4261acef14818c70f47700115bfd208d84783ce
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-09 12:25:03 +0530

    regular expressions

--- first website/js/first.js
+++ first website/js/first.js
@@ -1,3 +1,17 @@
 function myFunction(a, b) {
+    console.log("the answer is" +a*b)
     return a * b;
+    }
+
+    function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  {
+    return (true)
   }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+  
+   
