5a38900ccb1a4587e0a81c367043f676857c7d00
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-10 11:59:02 +0530

    passing proprties to components

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+import Greet from './components/Greet'
+import Welcome from './components/welcome'
 
 
 function App() {
   return (
     <div className="App">
-     <Greet/>
+    <Greet name = "priya" college = "IIT">
+     <p>this is children property </p>
+    </Greet>
+    <Greet name = "shiva" college = "Oxford"/>
+    <Greet name = "shivswetha"/>
+    
+     <Welcome/>
+
     </div>
   );
 }
--- dxcReact/dxcreactapp/src/components/Greet.js
+++ dxcReact/dxcreactapp/src/components/Greet.js
@@ -4,6 +4,15 @@ import React from 'react'
     return <h1>welcome surya</h1>
 } */
 
-const Greet = () => <h1>hello priya</h1>
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1> hello {props.name} you are from {props.college}</h1>
+            {props.children}
+        </div>
 
+
+    )
+}
 export default Greet
