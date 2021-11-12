3df8865f22c2469245ceee9c4b4d499bedaf1c91
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-12 10:47:47 +0530

    desturing a props

--- dxcReact/dxcreactapp/src/components/Greet.js
+++ dxcReact/dxcreactapp/src/components/Greet.js
@@ -4,13 +4,15 @@ import React from 'react'
     return <h1>welcome surya</h1>
 } */
 
-const Greet = (props) => {
-    console.log(props);
+//const Greet = ({name,college}) => {
+    const Greet = props => {
+        const {name,college} = props
+    //console.log(props);
     //props.name = 'dxc'
     return (
         <div>
-            <h1> hello {props.name} you are from {props.college}</h1>
-            {props.children}
+            <h1> hello {name} you are from {college}</h1>
+           
         </div>
 
 
