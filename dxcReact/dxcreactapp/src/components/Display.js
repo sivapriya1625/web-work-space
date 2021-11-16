af015f8ff2a1be12791dfa956ed05d7b6d25fb49
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-16 22:29:39 +0530

    button and display

---
+++ dxcReact/dxcreactapp/src/components/Display.js
@@ -0,0 +1,12 @@
+import "./Display.css";
+export default class Display extends React.Component {
+    static propTypes = {
+      value: PropTypes.string,
+    };
+  
+    render() {
+      return (
+        <div className="component-display">
+        
+              );
+  
