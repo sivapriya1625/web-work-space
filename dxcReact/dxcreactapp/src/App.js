443d4d20adaf84754f04813c4b14f602032335f4
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 16:49:13 +0530

    inline styling

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -5,12 +5,20 @@ import Table from './components/Table';
 import Stylesheet from './components/Stylesheet';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+
+
 function App() {
   return (
-    <div className="App"><Stylesheet  enabled = {true}/>
+    <div className="App">
+    <Inline/>
+    <Stylesheet  enabled = {true}/>
     <Table/>
     <NameList/>
-    <UserGreeting/></div>
+    </div>
   )
 }
 
--- dxcReact/dxcreactapp/src/components/styleSheet.js
+++ dxcReact/dxcreactapp/src/components/styleSheet.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './myStyles.css'
 
 function Stylesheet(props) {
+    //literal-$, template --{} and attribute-name
 
     let className = props.enabled ? 'primary' : ''
-    return (
-        <div><h1 className = {`${className} font-xl`}> welcome to style sheets</h1>
-        </div>
-         )
-        }
-        
+    return ({
+
+    })
+         
+}
