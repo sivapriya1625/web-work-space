26c5382a8ba10678b8b440e180d4486c78be7d79
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 17:17:55 +0530

    select component

--- dxcReact/dxcreactapp/src/components/Form.js
+++ dxcReact/dxcreactapp/src/components/Form.js
@@ -6,32 +6,55 @@ constructor(props) {
     super(props)
 
     this.state = {
-         username: '',
-         comments: ''
+        username: '',
+        comments: '',
+        topic: 'react'
     }
+} 
+handleUsernameChange = (event) => {
+
+}
+}
+handleCommentsChange = (event) => {
+    this.setState({
+        comments: event.target.value
+    })
 }
 
+handleTopicChange = (event) => {
+    this.setState({
+        topic: event.target.value
+    })
+}
 
-    handleUsernameChange = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-    }
-
-    render() {
-        return (
-            <form>
-                <div>
-                    <label>username</label>
-                    <input type='text' value ={this.state.username} onChange = {this.handleUsernameChange} />
+render() {
+    return (
+        <form>
+            <div>
+                <label>username</label>
+                <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
                 </div>
                 <div>
                     <label>comments</label>
-                    <textarea value = {this.state.comments }></textarea>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                </div>
+                <div>
+                    <label>topics</label>
+                    <select value = {this.state.topic} onChange = {this.handleTopicChange}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+
+                    </select>
                 </div>
             </form>
         )
     }
-}
+    
+
+               
+    
+    
+
 
 export default Form
