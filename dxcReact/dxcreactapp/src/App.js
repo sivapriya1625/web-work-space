ed4fda9b59d0aaff8ab73a099c0f0c218ecee738
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 16:21:41 +0530

    styling css

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/welcome'
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
 
 
 function App() {
   return (
     <div className="App">
-      <NameList/>
-     <UserGreeting/>
-
-      <ParentComponent/>
-      <EventBind/>
-
-    </div>
-  );
-}
-
-export default App;
+    <Stylesheet/>
+    <Table/>
+    <NameList/>
+    <UserGreeting/></div>
+    )
+  }
+    
+    
