d3ed6d27a746731b1aeb694055d8707086933bc4
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 16:55:32 +0530

    css modules

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -11,17 +11,24 @@ import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
 
 
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
+
 function App() {
   return (
     <div className="App">
+    <h1 className = {styles.success}>success</h1>
+    <h1 className = 'error'>error</h1>
+
     <Inline/>
     <Stylesheet  enabled = {true}/>
-    <Table/>
-    <NameList/>
-    </div>
-  )
-}
-
-
+    <Table/></div>
+   
+    )
+    }
 
     
