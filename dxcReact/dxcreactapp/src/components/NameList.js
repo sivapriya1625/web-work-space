4410f2ea33f9ff86d81b485b26b4bff21162c3f4
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 15:55:40 +0530

    list of persons json with multiple properties

---
+++ dxcReact/dxcreactapp/src/components/NameList.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function NameList() {
+    const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
+    
+        const persons = [
+            {
+                id:1,
+                name: 'abdul',
+                age: 30,
+                skill: 'react'
+            },
+            {
+                id:1,
+                name: 'ansari',
+                age: 30,
+                skill: 'flutter'
+            },
+            {
+                id:1,
+                name:'tanveer',
+                age: 30,
+                skill: 'java'
+            },
+            {
+                id:1,
+                name:'praneshwar',
+                age: 30,
+                skill: 'spring'
+            }
+        ]
+
+
+
+    /* const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
+    const namesList = names.map(name => <h2>{name}</h2>) */
+    const personList = persons.map(person => <h2> i am {person.name},i am {person.age} years old and my skill is {person.skill}</h2>)
+
+    return (
+       
+         React from 'react'
+
+        function NameList() {
+            const names = ['priya', 'shiva', 'swetha', 'dxc','react']
+            const namesList = names.map(name => <h2>{name}</h2>)
+                const persons = [
+                    {
+                        id:1,
+                        name: 'priya',
+                        age: 30,
+                        skill: 'react'
+                    },
+                    {
+                        id:1,
+                        name: 'shiva',
+                        age: 30,
+                        skill: 'flutter'
+                    },
+                    {
+                        id:1,
+                        name:'swetha',
+                        age: 30,
+                        skill: 'java'
+                    },
+                    {
+                        id:1,
+                        name:'guru',
+                        age: 30,
+                        skill: 'spring'
+                    }
+                ]
+        
+        
+        
+            /* const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
+            const namesList = names.map(name => <h2>{name}</h2>) */
+            const personList = persons.map(person => <h2> i am {person.name},i am {person.age} years old and my skill is {person.skill}</h2>)
+        
+            return (
+                
+                <div>{personList}</div>
+            )
+        }
+         }
+        
+        export default NameList
+
+ 
+
