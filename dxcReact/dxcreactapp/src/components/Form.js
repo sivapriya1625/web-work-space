253b4dd91233bc3a6002e12bb0a382be985f3142
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 17:22:58 +0530

    form submit button

--- dxcReact/dxcreactapp/src/components/Form.js
+++ dxcReact/dxcreactapp/src/components/Form.js
@@ -20,41 +20,35 @@ handleCommentsChange = (event) => {
         comments: event.target.value
     })
 }
-
-handleTopicChange = (event) => {
-    this.setState({
-        topic: event.target.value
-    })
+handleSubmit = (event) => {
+    alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+    event.preventDefault() //conents will not be refreshed/vanish
 }
 
 render() {
+    const {username, comments, topic} = this.state
     return (
-        <form>
-            <div>
-                <label>username</label>
-                <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+        <form onSubmit = {this.handleSubmit}>
+                <div>
+                    <label>username</label>
+                    <input type='text' value={username} onChange={this.handleUsernameChange} />
                 </div>
                 <div>
                     <label>comments</label>
-                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                    <textarea value={comments} onChange={this.handleCommentsChange}></textarea>
                 </div>
                 <div>
                     <label>topics</label>
-                    <select value = {this.state.topic} onChange = {this.handleTopicChange}>
+                    <select value = {topic} onChange = {this.handleTopicChange}>
                         <option value="react">React</option>
                         <option value="angular">Angular</option>
                         <option value="vue">Vue</option>
 
                     </select>
                 </div>
+                <button type = "submit" >Submit</button>
             </form>
         )
     }
-    
-
-               
-    
-    
-
 
 export default Form
