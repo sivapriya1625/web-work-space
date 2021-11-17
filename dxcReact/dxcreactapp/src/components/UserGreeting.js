08efc91c6103d8bebe3cd911f8f9d41ebad94707
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-17 12:35:49 +0530

    conditional rendering using varaible message

--- dxcReact/dxcreactapp/src/components/UserGreeting.js
+++ dxcReact/dxcreactapp/src/components/UserGreeting.js
@@ -5,17 +5,25 @@ export class UserGreeting extends Component {
         super(props)
     
         this.state = {
-             isLoggedIn: true
+             isLoggedIn: false
         }
     }
     
     render() {
+        let Message
         if(this.state.isLoggedIn){
+            Message = <div>welcome priya</div>
+        }
+        else{
+            Message = <div>welcome guest</div>
+        }
+        return <div>{Message}</div>
+        /* if(this.state.isLoggedIn){
             return <div>welcome priya</div>
         }
         else{
             return <div>welcome guest</div>
-        }
+        } */
         /* return (
             <div>
                 <div>welcome abdul</div>
