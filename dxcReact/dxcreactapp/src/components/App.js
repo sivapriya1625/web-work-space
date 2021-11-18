fdfa5fa35ddccc73d644972ae49fdafbcede0cae
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-18 10:57:17 +0530

    portals demo

--- dxcReact/dxcreactapp/src/components/App.js
+++ dxcReact/dxcreactapp/src/components/App.js
@@ -1,12 +1,13 @@
+import './App.css';
+import PortalDemo from './components/portals/PortalDemo';
 import FrParentInput from './components/refs/FrParentInput';
-
 
 
 function App() {
   return (
     <div className="App">
-  
-   <FrParentInput/>
-   </div>
- );
+    <FrParentInput/>
+    <PortalDemo/>
+    </div>
+  );
 }
