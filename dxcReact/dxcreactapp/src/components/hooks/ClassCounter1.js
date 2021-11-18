b240a33648c48fc4048d1496c9290016153a9dd0
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-18 12:15:04 +0530

    useEffect conditional run in functional component

--- dxcReact/dxcreactapp/src/components/hooks/ClassCounter1.js
+++ dxcReact/dxcreactapp/src/components/hooks/ClassCounter1.js
@@ -1,51 +1,28 @@
 import React, { Component } from 'react'
 
-/**
- * demo without using hook useEffect
- */
-export class ClassCounter1 extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0,
-          
-           name: ''
-       }
-   }
-
-   /**
-    * plug in the usb device -- effect --- didmount
-    */
-    componentDidMount() {
-        document.title = `clicked ${this.state.count} times`
-    }
-
-    /*
-        effect is didUpdate
-     */
-        componentDidUpdate(prevProps, prevState) {
-            if (prevState.count != this.state.count) {
-                console.log('updating doc title')
-                document.title = `clicked ${this.state.count} times`
-            }
-             //problem is every time i add a character to the input field, state is getting updated and component is being re rendered
-    //i want componentDidUpdate to be called only when count increases
-    render() 
+function CounterHook1() {
+    const [count,setCount] = useState(0)
+    const [name,setName] = useState('')
+    //use effect will be called after render method executes each time
+    useEffect(
+        ()=>{
+            console.log('updating -- useEffect hook')
+            document.title = `you clicked ${count} times`
+        },
+        [count] //hey update only when the count variable changes
+    )
         return (
-            <div><input
-            type='text'
-            value={this.state.name}
-            onChange={e => { this.setState({ name: e.target.value }) }}
-        />
-        <button
-            onClick={
-                () => this.setState({ count: this.state.count + 1 })
-            }>
-            click {this.state.count} times
-        </button>
-    </div>
+            <div>
+                <input
+                    type ='text'
+                    value = {name}
+                    onChange = {
+                        e => setName(e.target.value)
+                    }
+                />
+                <button onClick = { ()=> setCount(count+1)}>Click {count} times</button>
+            </div>
         )
-    }
     
    
     
