e8fc961e1ccaaa6217bd5f9b5a0edcb0d24605ff
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-18 12:09:06 +0530

    updating conditionally

--- dxcReact/dxcreactapp/src/components/hooks/ClassCounter1.js
+++ dxcReact/dxcreactapp/src/components/hooks/ClassCounter1.js
@@ -6,38 +6,50 @@ import React, { Component } from 'react'
 export class ClassCounter1 extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
-             count: 0
-        }
-    }
+            count: 0,
+          
+           name: ''
+       }
+   }
 
-    /**
-     * plug in the usb device -- effect --- didmount
-     */
-    componentDidMount(){
+   /**
+    * plug in the usb device -- effect --- didmount
+    */
+    componentDidMount() {
         document.title = `clicked ${this.state.count} times`
     }
 
     /*
         effect is didUpdate
      */
-    componentDidUpdate(prevProps,prevState){
-        document.title = `clicked ${this.state.count} times`
-    }
-
-    render() {
+        componentDidUpdate(prevProps, prevState) {
+            if (prevState.count != this.state.count) {
+                console.log('updating doc title')
+                document.title = `clicked ${this.state.count} times`
+            }
+             //problem is every time i add a character to the input field, state is getting updated and component is being re rendered
+    //i want componentDidUpdate to be called only when count increases
+    render() 
         return (
-            <div>
-                <button 
-                onClick ={ 
-                    () => this.setState({count: this.state.count + 1})
-                }>
-                click {this.state.count} times
-                </button>
-            </div>
+            <div><input
+            type='text'
+            value={this.state.name}
+            onChange={e => { this.setState({ name: e.target.value }) }}
+        />
+        <button
+            onClick={
+                () => this.setState({ count: this.state.count + 1 })
+            }>
+            click {this.state.count} times
+        </button>
+    </div>
         )
     }
-}
+    
+   
+    
 
-export default ClassCounter1
+        
+
+
