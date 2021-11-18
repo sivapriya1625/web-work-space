48c7963034291d86faa662562ab669bffb08a5c0
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-18 11:48:20 +0530

    useState with object and spread operator

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import HookCounter2 from './components/hooks/HookCounter2';
+import HookCounter3 from './components/hooks/HookCounter3';
 
 
-
-
-
+import HookCounter2 from './components/hooks/HookCounter2';
 function App() {
   return (
     <div className="App">
-         <HookCounter2/>
+       <HookCounter3/>
     </div>
   );
 }
-
-export default App
