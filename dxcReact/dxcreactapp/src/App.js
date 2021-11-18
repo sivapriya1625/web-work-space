70c55f1f56c17abb089dc9f947eb299de11c3b13
sivapriya J <sivapriya.jammala@gmail.com>
2021-11-18 11:52:18 +0530

    useEffect hook to update title

--- dxcReact/dxcreactapp/src/App.js
+++ dxcReact/dxcreactapp/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import HookCounter2 from './components/hooks/HookCounter2';
-import HookCounter3 from './components/hooks/HookCounter3';
-
-
-import HookCounter2 from './components/hooks/HookCounter2';
+import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
+import CounterHook1 from './components/hooks/useeffect/CounterHook1';
+ import HookCounter3 from './components/hooks/HookCounter3';
 function App() {
   return (
     <div className="App">
-       <HookCounter3/>
+      <CounterHook1/>
     </div>
   );
 }
